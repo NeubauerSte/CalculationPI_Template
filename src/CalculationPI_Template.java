2491b305852df4c2d520ac1c35b04d5b61e0feec
gerhard.lanzenberger@htl-wels.at <gerhard.lanzenberger@htl-wels.at>
2020-10-22 13:10:13 +0200

    description of task only

---
+++ src/CalculationPI_Template.java
@@ -0,0 +1,29 @@
+public class CalculationPI_Template {
+
+      /*
+    erstellt von Gerhard L.
+    erstellt am 18.10.2020
+    Beschreibung: berechnet die Zahl Pi mit unterschiedlichen Näherungsalgorithmen
+                  zusätzlich wird dem User eine Sortierung der Ergebnisse angeboten
+     */
+
+    public static void main(String[] args) {
+
+    //Erstellgung der Objekte für die Berechnung von PI
+    //bereits in letzter Übung umgesetzt
+    //...
+
+
+    /*
+    AUFGABENSTELLUNG: erstellen Sie eine Methode zum Vergleich der Algorithmen
+
+    absteigend sortiert, d.h. das Ergebnis, das Pi am nächsten kommt, zuerst
+    beachten Sie negative und positive Differenzen, arbeiten Sie mit absoluten Zahlen
+    legen Sie sich eine Referenzzahl für PI als Konstante an
+    versuchen Sie es möglichst einfach (Lösung mit compareTo möglich?)
+
+    Denken Sie an eine objektorientierte Lösung!
+    */
+
+    }
+}
