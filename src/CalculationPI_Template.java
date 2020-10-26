772249f86b26b767747b818d2e51e05117a5d6f5
NeubauerSte <stefan04.neubauer@gmail.com>
2020-10-26 15:21:46 +0100

    V1 - old project implemented

--- src/CalculationPI_Template.java
+++ src/CalculationPI_Template.java
@@ -1,18 +1,37 @@
-public class CalculationPI_Template {
 
-      /*
+import java.util.InputMismatchException;
+import java.util.Scanner;
+ /*
     erstellt von Gerhard L.
     erstellt am 18.10.2020
     Beschreibung: berechnet die Zahl Pi mit unterschiedlichen Näherungsalgorithmen
                   zusätzlich wird dem User eine Sortierung der Ergebnisse angeboten
+
+
+     - Überarbeitet von Stefan Neubauer, am 26.10.2020
      */
 
+public class CalculationPI_Template {
     public static void main(String[] args) {
 
-    //Erstellgung der Objekte für die Berechnung von PI
-    //bereits in letzter Übung umgesetzt
-    //...
+        Scanner sc = new Scanner(System.in);
+        int basis=0;
 
+        try { // Exceptionhandling
+            System.out.println("Wie oft soll multipliziert werden für PI?");
+            basis = sc.nextInt(); }
+        catch (InputMismatchException iem){ System.out.println("Bitte eine ZAHL angeben!"); System.exit(0); }
+
+
+        CalculateAll calculateEuler = new CalculateEuler();
+        CalculateAll calculateKellalur = new CalculateKellalur();
+        CalculateAll calculateLeibnitz = new CalculateLeibnitz();
+        CalculateAll calculateWallprodukt = new CalculateWallprodukt();
+
+        System.out.println(calculateEuler.toString(basis));
+        System.out.println(calculateKellalur.toString(basis));
+        System.out.println(calculateLeibnitz.toString(basis));
+        System.out.println(calculateWallprodukt.toString(basis));
 
     /*
     AUFGABENSTELLUNG: erstellen Sie eine Methode zum Vergleich der Algorithmen
